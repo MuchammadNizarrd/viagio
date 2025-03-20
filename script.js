32821cf4ac09e86e700fca76de06576d05d9cafe
muchammadnizard <nizardanu1@gmail.com>
2025-03-20 13:55:25 +0700

    no bug

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var tumpakSewuBtn = document.getElementById("tumpak-sewu-btn");
     var videoContainer = document.getElementById("tiktok-video-container");
     var closeVideo = document.getElementById("close-video");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     tumpakSewuBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
@@ -39,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var gunungbuthakBtn = document.getElementById("gunung-buthak-btn");
     var videoContainer = document.getElementById("tiktok-video-buthak");
     var closeVideo = document.getElementById("close-video-buthak");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     gunungbuthakBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
@@ -55,6 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var banyumenengBtn = document.getElementById("banyu-meneng-btn");
     var videoContainer = document.getElementById("tiktok-video-banyu");
     var closeVideo = document.getElementById("close-video-banyu");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     banyumenengBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
@@ -70,6 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var budugasuBtn = document.getElementById("budug-asu-btn");
     var videoContainer = document.getElementById("tiktok-video-budug");
     var closeVideo = document.getElementById("close-video-budug");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     budugasuBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
@@ -85,6 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var ranureguloBtn = document.getElementById("ranu-regulo-btn");
     var videoContainer = document.getElementById("tiktok-video-ranu");
     var closeVideo = document.getElementById("close-video-ranu");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     ranureguloBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
@@ -100,6 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var privateBtn = document.getElementById("private-trip-btn");
     var videoContainer = document.getElementById("tiktok-video-private");
     var closeVideo = document.getElementById("close-video-private");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     privateBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
@@ -115,6 +121,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var privateBtn = document.getElementById("pantai-wonogoro-btn");
     var videoContainer = document.getElementById("tiktok-video-wonogoro");
     var closeVideo = document.getElementById("close-video-wonogoro");
+    var tiktokVideo = videoContainer.querySelector("iframe");
 
     privateBtn.addEventListener("click", function () {
         videoContainer.style.display = "block";
